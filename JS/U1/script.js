0818dd486f444b2ee38f9eedfa18550c4690811d
Gonzalo Nahuel Gómez <62256944+gonzalonahuelgomez@users.noreply.github.com>
2022-04-30 02:33:53 +0000

    finish js exercise 1

--- JS/U1/script.js
+++ JS/U1/script.js
@@ -1,3 +1,5 @@
 const primera = 40, segunda = 16, sumaTotal = primera + segunda
 
-document.getElementById("suma").innerHTML = `El resultado de la suma de las dos variables es:\n${sumaTotal}`
+document
+    .getElementById("suma")
+    .innerHTML = `<p>El resultado de la suma de las dos variables es:<p class="valor">${sumaTotal}</p></p>`
